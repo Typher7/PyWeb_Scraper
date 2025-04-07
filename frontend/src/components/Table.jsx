cbe9ba8e237ef05bd4a378e1d0b8b2a9239f5091
Enoch Atuahene <enatua01@wsc.edu>
2025-04-07 12:13:02 -0500

    fixed table overlay issue

--- frontend/src/components/Table.jsx
+++ frontend/src/components/Table.jsx
@@ -37,8 +37,8 @@ export default function Table({ priceHistory, onClose }) {
         <div className="sm:flex-auto">
           <h1 className="text-xl font-semibold text-gray-900">Price History</h1>
           <p className="mt-2 text-sm text-gray-700">
-            A list of all products matching the search including their updated time, current price,
-            and price change.
+            A list of all products matching the search including their updated
+            time, current price, and price change.
           </p>
         </div>
       </div>
@@ -54,25 +54,25 @@ export default function Table({ priceHistory, onClose }) {
                   <tr>
                     <th
                       scope="col"
-                      className="sticky top-0 z-10 border-b border-gray-300 bg-gray-50 bg-opacity-75 py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:pl-6 lg:pl-8"
+                      className="sticky top-0 z-1 border-b border-gray-300 bg-gray-50 bg-opacity-75 py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:pl-6 lg:pl-8"
                     >
                       Description
                     </th>
                     <th
                       scope="col"
-                      className="sticky top-0 z-10 hidden border-b border-gray-300 bg-gray-50 bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:table-cell"
+                      className="sticky top-0 z-1 hidden border-b border-gray-300 bg-gray-50 bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:table-cell"
                     >
                       Update at
                     </th>
                     <th
                       scope="col"
-                      className="sticky top-0 z-10 hidden border-b border-gray-300 bg-gray-50 bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter lg:table-cell"
+                      className="sticky top-0 z-1 hidden border-b border-gray-300 bg-gray-50 bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter lg:table-cell"
                     >
                       Price
                     </th>
                     <th
                       scope="col"
-                      className="sticky top-0 z-10 border-b border-gray-300 bg-gray-50 bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter"
+                      className="sticky top-0 z-1 border-b border-gray-300 bg-gray-50 bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter"
                     >
                       Price Change
                     </th>
@@ -84,57 +84,63 @@ export default function Table({ priceHistory, onClose }) {
                     const change = getPriceChange(product);
 
                     return (
-                    <tr key={product.url}>
-                      <td
-                        className={classNames(
-                          productIdx !== product.length - 1
-                            ? "border-b border-gray-200"
-                            : "",
+                      <tr key={product.url}>
+                        <td
+                          className={classNames(
+                            productIdx !== product.length - 1
+                              ? "border-b border-gray-200"
+                              : "",
                             "whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 lg:pl-8 truncate max-w-xs"
-                          
-                        )}
-                        title={product.name}
-                      >
-                        <a onClick={() => openModal(product)}>{product.name}</a>
-                      </td>
-                      <td
-                        className={classNames(
-                          productIdx !== product.length - 1
-                            ? "border-b border-gray-200"
-                            : "",
+                          )}
+                          title={product.name}
+                        >
+                          <a onClick={() => openModal(product)}>
+                            {product.name}
+                          </a>
+                        </td>
+                        <td
+                          className={classNames(
+                            productIdx !== product.length - 1
+                              ? "border-b border-gray-200"
+                              : "",
                             "whitespace-nowrap px-3 py-4 text-sm text-gray-500 hidden sm:table-cell "
-                        )}
-                      >
-                        {priceData.date}
-                      </td>
-                      <td
-                        className={classNames(
-                          productIdx !== product.length - 1
-                            ? "border-b border-gray-200"
-                            : "",
-                          "whitespace-nowrap px-3 py-4 text-sm text-gray-500 hidden lg:table-cell"
-                        )}
-                      >
-                        ${priceData.price}
-                      </td>
-                      <td
-                        className={classNames(
-                          productIdx !== product.length - 1
-                            ? "border-b border-gray-200"
-                            : "",
-                          "whitespace-nowrap px-3 py-4 text-sm text-gray-500",
-                          change > 0 ? "text-red-500" : "text-green-500"
-                        )}
-                      >
-                        {change >= 0 && "+" }
-                        {change}%
-                      </td>
-                    </tr>
+                          )}
+                        >
+                          {priceData.date}
+                        </td>
+                        <td
+                          className={classNames(
+                            productIdx !== product.length - 1
+                              ? "border-b border-gray-200"
+                              : "",
+                            "whitespace-nowrap px-3 py-4 text-sm text-gray-500 hidden lg:table-cell"
+                          )}
+                        >
+                          ${priceData.price}
+                        </td>
+                        <td
+                          className={classNames(
+                            productIdx !== product.length - 1
+                              ? "border-b border-gray-200"
+                              : "",
+                            "whitespace-nowrap px-3 py-4 text-sm text-gray-500",
+                            change > 0 ? "text-red-500" : "text-green-500"
+                          )}
+                        >
+                          {change >= 0 && "+"}
+                          {change}%
+                        </td>
+                      </tr>
                     );
                   })}
                 </tbody>
               </table>
-              <button className="bg-red-400 text-white py-1 px-1 m-2 rounded-lg hover:bg-red-500 transition-all" onClick={onClose}>Close</button>
+              <button
+                className="bg-red-400 text-white py-1 px-1 m-2 rounded-lg hover:bg-red-500 transition-all"
+                onClick={onClose}
+              >
+                Close
+              </button>
               <ModalComponent
                 isOpen={isModalOpen}
                 closeModal={closeModal}
